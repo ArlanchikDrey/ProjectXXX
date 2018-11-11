44031c2b5823460d4c840f850e222740a3b21d68
ArlanchikDrey <arlanchikdrey@yandex.ru>
2018-11-11 22:52:19 +0300

    ProjectXXX

---
+++ app/src/main/java/com/example/projectxxx/QuestionPackages/QuestionMainFragment.java
@@ -0,0 +1,23 @@
+package com.example.projectxxx.QuestionPackages;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.projectxxx.R;
+
+import java.util.zip.Inflater;
+
+public class QuestionMainFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView( LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_question_main,container,false);
+        return view;
+    }
+}
